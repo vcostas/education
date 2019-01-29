5a9d65c21a1204dbcd3afed34337c37b3cdbde89
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2019-01-29 14:42:30 -0300

    Updated views and url structure.

--- sistemaDeNotas/correo/urls.py
+++ sistemaDeNotas/correo/urls.py
@@ -1,12 +1,14 @@
 from . import views
-from django.urls import path
+from django.urls import path, include
+
+app_name='correo'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('inbox', views.inbox, name='inbox'),
-    path('compose', views.compose, name='compose'),
-    path('sent', views.sent, name='sent'),
-    path('drafts', views.drafts, name='drafts'),
-    path('junk', views.junk, name='jumk'),
-    path('trash', views.trash, name='trash'),
+    path('inbox/', views.inbox, name='inbox'),
+    path('compose/', views.compose, name='compose'),
+    path('sent/', views.sent, name='sent'),
+    path('drafts/', views.drafts, name='drafts'),
+    path('junk/', views.junk, name='junk'),
+    path('trash/', views.trash, name='trash'),
 
 ]
--- sistemaDeNotas/correo/views.py
+++ sistemaDeNotas/correo/views.py
@@ -1,14 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
 from django.http import HttpResponse
-# from sitemessage.toolbox import schedule_messages, recipients
-# from sitemessage.shortcuts import schedule_email
-# from sitemessage.toolbox import recipients, send_scheduled_messages
+from django.shortcuts import render
+
 from sitemessage.messengers.smtp import SMTPMessenger
 from sitemessage.messengers.mymessenger import MyMessenger
-
 from sitemessage.models import Dispatch, Message
-from django.contrib.auth.user import User
-# Create your views here.
+from sitemessage.shortcuts import schedule_email
+# from sitemessage.toolbox import schedule_messages, recipients
+# from sitemessage.toolbox import recipients, send_scheduled_messages
 
 
 def index(request):
@@ -18,43 +17,51 @@ def inbox(request):
     """
     Retrieves all the messages for the logged in user and renders the admin Mailbox template.
     """
-    inboxMessages = Dispatch.objects.filter(recipient_id=request.user.id)
-    
-    return HttpResponse("Hello, {}. You have sent the following messages {}.".format((request.user.username, inboxMessages)))
+    inboxMessagesId = Dispatch.objects.filter(recipient_id=request.user.id)
+    inboxMessages = []
+    for m in inboxMessagesId:
+        msg = Message.objects.get(id=m.message_id)
+        inboxMessages.append(msg.context)
+    return HttpResponse(inboxMessages)
+    # return render(request, 'correo/read-mail.html', context=context)
     
 def sent(request):
     """
     Retrives all the messages sent to the logged in user and renders the admin Mailbox template.
     """
-    sentMessages = Message.objects.filter(sender = request.user.id)
-
-    return HttpResponse("Hello, {}. You have sent the following messages {}.".format((request.user.username, sentMessages)))
-
+    sentMessages = Message.objects.filter(sender_id = request.user.id).values('context')
+    return HttpResponse(sentMessages)
+    #render(request, 'correo/mailbox.html', {})
+    
 def compose(request):
     """
     Allows logged in user to write a new message for another user in the system.
     """
+    context = {}
+    render(request, 'correo/compose.html', context=context)
 
-    return HttpResponse("Hello, {}. You're at the sitemessage COMPOSE!.".format(request.user.username))
 
 def drafts(request):
     """
     Creates a message and stores it. These messages have not been dispatched yet.
     """
-
-    return HttpResponse("Hello, {}. You're at the sitemessage DRAFTS!.".format(request.user.username))
-
+    draftMessages = Message.objects.filter(sender_id = request.user.id).values('context')
+    return HttpResponse(draftMessages)
+    # render(request, 'correo/mailbox.html', context=context)
+    
 def junk(request):
     """
     Returns all the messages which are marked deleted by the logged in user.
     """
-    return HttpResponse("Hello, {}. You're at the sitemessage JUNK!.".format(request.user.username))
+    context = {}
+    render(request, 'correo/mailbox.html', context=context)
 
 def trash(request):
     """
     Returns all the messages which are marked deleted by the logged in user.
     """
-    return HttpResponse("Hello, {}. You're at the sitemessage TRASH!.".format(request.user.username))
+    context = {}
+    render(request, 'correo/mailbox.html', context=context)
 
 def send_messages_view(request, id):
     """
@@ -64,17 +71,14 @@ def send_messages_view(request, id):
     
     to_user = User.objects.get(pk=id)
     sender = request.user
-
+    
     # Here we define the smtp recipients to inform them via email that
     # they have an unread notification.
     my_smtp_recipients = recipients(SMTPMessenger, [to_user.email]),
 
     # Here we define the internal mail recipients 
     my_recipients = recipients(MyMessenger, [to_user])
-    
-       
-       
-    
-    schedule_email('There is hope.', [to_user, request.user])
+      
+    schedule_email('There is hope.', [to_user, sender.username])
     schedule_email('You have an unread notificatoin in eduanalytics.',[my_smtp_recipients, sender])
     
